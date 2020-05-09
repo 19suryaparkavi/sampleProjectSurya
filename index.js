7e67621aaf101b667844b3da5c97c70ad268855b
surya parkavi <65016796+19suryaparkavi@users.noreply.github.com>
2020-05-09 18:25:27 +0530

    Add files via upload
    Master File

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json()); // creates http server
+const token = 'VERIFICATION_TOKEN'; // type here your verification token
+
+app.listen(3000, () => console.log('[ChatBot] Webhook is listening'));
